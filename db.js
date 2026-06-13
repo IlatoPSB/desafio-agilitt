2efaeac0bc1bb08556fea9db126dcd54e2e8df04
Italo <italo.psb@gmail.com>
2026-06-13 12:46:22 -0300

    Corrige conexão com MySQL usando pool

--- db.js
+++ db.js
@@ -1,20 +1,16 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
-const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
-connection.connect((err) => {
-    if (err) {
-        console.error('Erro ao conectar:', err);
-        return;
-    }
+console.log('Pool MySQL criado com sucesso!');
 
-    console.log('Conectado ao MySQL!');
-});
-
-module.exports = connection;
+module.exports = pool;
